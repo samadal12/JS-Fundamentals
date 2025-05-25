7e08ac274d316eadcd196eb5abe28949be9de8bb
Mac <samed-mac@192.168.1.34>
2025-05-25 19:31:59 +0100

    8-square.js

---
+++ 8-square.js
@@ -0,0 +1,11 @@
+
+const size = Number(process.argv[2]);
+
+if (!Number.isInteger(size)) {
+  console.log("Missing size");
+} else {
+  const line = "X".repeat(size);
+  for (let i = 0; i < size; i++) {
+    console.log(line);
+  }
+}
